5be4d34f98279f53b9bbeed3a4f287a2c9a4c1c8
Felipe <fmagesty@gmail.com>
2022-07-11 18:36:45 -0300

    fea: add initial env var handling f/ mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvPath } from './common/helper/env.helper';
+import { ConfigModule } from '@nestjs/config';
+// import { MongooseModule } from '@nestjs/mongoose';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  // Uncomment next line to use MongooseModule instead of ConfigModule
+  // imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
